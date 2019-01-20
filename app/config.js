d630ecc1f066377f02d5ba3253151064644ea015
igloosi <jani.kraner@gmail.com>
2019-01-20 19:37:46 +0000

    V1 - sprint 1

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Surrender emissions allowance for ETS',
 
   // Default port that prototype runs on
   port: '3000',
--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.get('/*-submit', function(req, res, next){
+  res.locals['serviceName'] = 'Submit emissions data for ETS'
+  next()
+})
 
 module.exports = router
