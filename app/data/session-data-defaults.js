f0bb659f7a0413c458c06ac90108128e9522a5f2
igloosi <jani.kraner@gmail.com>
2019-01-20 19:57:24 +0000

    V3 - Sprints 3 & 4

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -20,5 +20,124 @@ Example usage:
 module.exports = {
 
   // Insert values here
-
+  'installations': [
+    {
+      'name': 'Nevern Power Limited',
+      'permitId': 'EU-100-73432-0-76',
+      'emissionAllowance': [
+        {
+          'general-allowance': 52834
+        }
+      ]
+    },
+    {
+      'name': 'Scunthorpe Integrated Iron & Steel Works',
+      'permitId': 'EU-100-82345-0-76',
+      'emissionAllowance': [
+        {
+          'general-allowance': 23871,
+          'CER': 138,
+          'ERU': 20
+        }
+      ]
+    },
+    {
+      'name': 'Port Talbot Steelworks',
+      'permitId': 'EU-100-73104-0-76',
+      'emissionAllowance': [
+        {
+          'general-allowance': 1406
+        }
+      ]
+    },
+    {
+      'name': 'Lynemouth Power Station',
+      'permitId': 'EU-100-77732-0-76',
+      'emissionAllowance': [
+        {
+          'general-allowance': 10473
+        }
+      ]
+    }
+  ],
+  'existing-authorised-representatives': [
+    {
+      name: 'Divina Glaser',
+      id: 'EU-66-5684-0-20'
+    },
+    {
+      name: 'Luann Steppe',
+      id: 'EU-66-5684-0-21'
+    },
+    {
+      name: 'Jimmy Pressly',
+      id: 'EU-66-5684-0-22'
+    },
+    {
+      name: 'Tyra Lucas',
+      id: 'EU-66-5684-0-23'
+    },
+    {
+      name: 'Terisa Pritchett',
+      id: 'EU-66-5684-0-24'
+    },
+    {
+      name: 'Cecille Irving',
+      id: 'EU-66-5684-0-25'
+    },
+    {
+      name: 'Elna Kraushaar',
+      id: 'EU-66-5684-0-26'
+    },
+    {
+      name: 'Luella Hover',
+      id: 'EU-66-5684-0-27'
+    },
+    {
+      name: 'Eloisa Albert',
+      id: 'EU-66-5684-0-28'
+    },
+    {
+      name: 'Fatimah Huffines',
+      id: 'EU-66-5684-0-29'
+    }
+  ],
+  'existing-accounts': [
+    {
+      name: 'National Grid Gas plc',
+      id: 'EU-100-8976-0-88'
+    },
+    {
+      name: 'Conoco Phillips',
+      id: 'EU-100-82543-0-87'
+    },
+    {
+      name: 'Dalkia Utilities Services',
+      id: 'EU-100-625343-0-99'
+    },
+    {
+      name: 'Perenco UK Ltd',
+      id: 'EU-100-11674-0-56'
+    },
+    {
+      name: 'Polimeri Europa UK Limited',
+      id: 'EU-100-77732-0-76'
+    },
+    {
+      name: 'De La Rue International Ltd',
+      id: 'EU-100-63522-0-03'
+    },
+    {
+      name: 'E.ON UK CHP Ltd',
+      id: 'EU-100-99924-0-23'
+    },
+    {
+      name: 'Citigen (London) Ltd',
+      id: 'EU-100-38476-0-24'
+    },
+    {
+      name: 'Procter & Gamble Product Supply (UK) Ltd',
+      id: 'EU-100-4847-0-25'
+    }
+  ]
 }
--- app/routes.js
+++ app/routes.js
@@ -1,50 +1,154 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
-router.get('/*-submit', function(req, res, next){
-  res.locals['serviceName'] = 'Submit emissions data for ETS'
+// make available for all routes
+router.use(function (req, res, next) {
+  if (!req.session.data.newAuthorisedReps) {
+    req.session.data.newAuthorisedReps = []
+  }
+  if (!req.session.data.newTrustedAccounts) {
+    req.session.data.newTrustedAccounts = []
+  }
   next()
 })
-router.get('/*-transfer', function(req, res, next){
+
+router.get('/apply-for-an-account/:page', function (req, res, next) {
+  res.locals['serviceName'] = 'Apply for an ETS account'
+  next()
+})
+
+router.get('/register-for-ets/:page', function (req, res, next) {
+  res.locals['serviceName'] = 'Register for ETS'
+  next()
+})
+
+router.get('/transfer-allowance/:page', function (req, res, next) {
   res.locals['serviceName'] = 'Check and trade emissions for ETS'
   next()
 })
 
-router.post("/choose-transfer-route", function(req,res){
-
-  var whichTransfer = req.session.data['whichtransfer']
-
-  if (whichTransfer == "Existing installation"){
-    res.redirect("/existing-transfer")
-  } else {
-    res.redirect("/new-transfer")
-  }
-
+router.get('/submit-emissions/:page', function (req, res, next) {
+  res.locals['serviceName'] = 'Submit emissions data for ETS'
+  next()
 })
 
-router.post("/choose-installation-route", function(req,res){
-
-  var installationName = req.session.data['installationName']
-
-  if (installationName == "Scunthorpe Integrated Iron & Steel Works"){
-    res.redirect("/choose-unit-transfer")
-  } else {
-    res.redirect("/choose-transfer")
-  }
-
+router.get('/surrender-allowance/:page', function (req, res, next) {
+  res.locals['serviceName'] = 'Surrender emissions allowance for ETS'
+  next()
 })
 
-router.post("/amount-surrender-route", function(req,res){
+router.get('/account/:id', function (req, res, next) {
+  res.locals['serviceName'] = 'Account'
+  res.locals['currentDate'] = Date.now()
+  res.locals['installationID'] = req.params.id
+  res.locals['pageView'] = req.query.view
+  res.render('account/index')
+})
 
-  var amountToSurrender = req.session.data['surrender-radio']
+router.get('/add-a-new-authorised-representative/:page', function (req, res, next) {
+  res.locals['serviceName'] = 'New authorised representative'
+  next()
+})
 
-  if (amountToSurrender == "Everything"){
-    res.redirect("/confirmation-surrender")
-  } else if (amountToSurrender == "Other") {
-    res.redirect("/are-you-sure-surrender")
+router.get('/add-a-new-trusted-account/:page', function (req, res, next) {
+  res.locals['serviceName'] = 'New trusted account'
+  next()
+})
+
+router.get('/add-a-new-trusted-account/account-details', function (req, res, next) {
+  if (req.query.error) {
+    res.locals['errorExists'] = req.query.error
   }
+  next()
+})
 
+router.post('/transfer-allowance/select-recipient-answer', function (req, res) {
+  var recipientType = req.session.data['ets-transfer-allowance']['select-recipient']
+
+  if (recipientType === 'existing') {
+    res.redirect('existing-recipient')
+  } else {
+    res.redirect('new-recipient')
+  }
+})
+
+router.post('/register-for-ets/account-details-answer', function (req, res) {
+  let previousEtsUser = req.session.data['ets-register']['is-previous-ets-user']
+
+  if (previousEtsUser === 'yes') {
+    res.redirect('/register-for-ets/your-linked-representative')
+  } else {
+    res.redirect('/apply-for-an-account/')
+  }
+})
+
+router.post('/register-for-ets/operator-selection-answer', function (req, res) {
+  let isGHG = req.session.data['ets-register']['ghg-operator']
+
+  if (isGHG === 'yes') {
+    res.redirect('/register-for-ets/account-details')
+  } else {
+    res.redirect('/apply-for-an-account/')
+  }
+})
+
+router.post('/register-for-ets/linked-representative-answer', function (req, res) {
+  let linkedReps = req.session.data['ets-register']['linked-reps']
+  if (linkedReps.includes('non-existant')) {
+    res.redirect('/register-for-ets/add-new-linked-representative')
+  } else {
+    res.redirect('/register-for-ets/check-and-submit')
+  }
+})
+
+router.post('/surrender-allowance/surrender-amount-answer', function (req, res) {
+  var amountToSurrender = req.session.data['ets-surrender-allowance']['amount-to-surrender']
+
+  if (amountToSurrender === 'other') {
+    res.redirect('confirm-oversurrender')
+  } else {
+    res.redirect('confirmation')
+  }
+})
+
+router.post('/add-a-new-authorised-representative/rep-details-answer', function (req, res, next) {
+  var isExistingEtsUser = req.session.data['new-linked-representative']['existing-ets-user']
+  var existingUserId = req.session.data['new-linked-representative']['id']
+
+  if (isExistingEtsUser === 'yes' && existingUserId !== ' ') {
+    res.redirect('check-respresentative-details')
+  } else {
+    res.redirect('confirmation')
+  }
+})
+
+router.post('/add-a-new-authorised-representative/confirmation', function (req, res, next) {
+  var newAuthorisedRepName = req.session.data['new-authorised-representatives']['name']
+  var newAuthorisedReps = req.session.data.newAuthorisedReps
+  if (!newAuthorisedReps.includes(newAuthorisedRepName)) {
+    req.session.data.newAuthorisedReps.push(newAuthorisedRepName)
+  }
+  next()
+})
+
+router.post('/add-a-new-trusted-account/account-details-answer', function (req, res, next) {
+  var newTrustedAccountId = req.session.data['new-trusted-account']['id'] || ' '
+  var doesItemExist = req.session.data['existing-accounts'].find(o => o.id === newTrustedAccountId) || false
+
+  if (newTrustedAccountId !== ' ' && doesItemExist) {
+    res.redirect('check-account-details')
+  } else {
+    res.redirect('/add-a-new-trusted-account/account-details?error=true')
+  }
+})
+
+router.post('/add-a-new-trusted-account/confirmation', function (req, res, next) {
+  var newTrustedAccount = req.session.data['new-trusted-account']['name']
+  var newTrustedAccounts = req.session.data.newTrustedAccounts
+  if (!newTrustedAccounts.includes(newTrustedAccount)) {
+    req.session.data.newTrustedAccounts.push(newTrustedAccount)
+  }
+  next()
 })
 
 module.exports = router
