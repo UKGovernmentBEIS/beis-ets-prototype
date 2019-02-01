0089ac92c59c7c41fe9e743ee9c72d34c5ab11bf
Chris Neale <christopher.neale@gmail.com>
2019-02-01 15:13:47 +0000

    Change name to UK Emission Trading System

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Emissions Trading System',
+  serviceName: 'UK Emissions Trading System',
 
   // Default port that prototype runs on
   port: '3000',
